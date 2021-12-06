8a95e951cf9e1b3a68e23e3fc768eab0f948718d
Sachin Rana <sachinrana1827@gmail.com>
2021-12-06 16:58:01 +0530

    updated header to add signin and account button

--- components/header.tsx
+++ components/header.tsx
@@ -1,19 +1,29 @@
 import styles from './styles/header.module.scss';
 import { useRouter } from 'next/router';
+import { auth } from '../lib/firebase';
+import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+import { useAuthState } from 'react-firebase-hooks/auth';
 
 export default function Header() {
+  const [user, loading, error] = useAuthState(auth);
   const router = useRouter();
+  const SignIn = () => {
+    signInWithPopup(auth, new GoogleAuthProvider());
+  };
   return (
-    <>
-      <div className={styles.header}>
-        <div onClick={() => router.push('/')}>Novel WEBAPP</div>
-        <div
-          onClick={() => router.push('/add-novel')}
-          className={styles.createNovelButton}
-        >
-          Create Novel
-        </div>
-      </div>
-    </>
+    <div className={styles.header}>
+      <div onClick={() => router.push('/')}>Novel WEBAPP</div>
+      {user ? (
+        <input
+          className={styles.user}
+          type='image'
+          src={user.photoURL!}
+          alt='user'
+          onClick={() => router.push('/user')}
+        />
+      ) : (
+        <button onClick={SignIn}>SignIn</button>
+      )}
+    </div>
   );
 }
