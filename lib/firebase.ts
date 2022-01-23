f1f24afd14f11ff42817d124a5a2e0639328dad2
Sachin Rana <sachinrana1827@gmail.com>
2022-01-23 20:32:40 +0530

    updated environment variable names

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,15 +1,14 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 export const app = initializeApp({
-  apiKey: process.env.NEXT_PUBLIC_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
-  appId: process.env.NEXT_PUBLIC_API_ID,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_API_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
 });
 
-export const firestore = getFirestore(app);
 export const auth = getAuth(app);
 export const storage = getStorage(app);
