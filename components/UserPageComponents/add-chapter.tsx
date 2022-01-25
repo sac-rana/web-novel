bfbc4f3d0b6bd1e165f4b1a858bb3dacc1c36a6f
Sachin Rana <sachinrana1827@gmail.com>
2022-01-25 18:57:09 +0530

    updated chapter addition process

--- components/UserPageComponents/add-chapter.tsx
+++ components/UserPageComponents/add-chapter.tsx
@@ -1,48 +1,44 @@
 import { FormEventHandler, useState } from 'react';
+import Router from 'next/router';
 
 export default function AddChapter({
-  chapterNo,
-  novel,
-  close,
+  novel: { titleSlug, title, chapterNo },
 }: {
-  chapterNo: number;
-  novel: { id: string; title: string };
-  close: Function;
+  novel: { titleSlug: string; title: string; chapterNo: number };
 }) {
   const [chapterContent, setChapterContent] = useState('');
 
   const addChapter: FormEventHandler = async e => {
     e.preventDefault();
-
-    document.location.reload();
+    await fetch('/api/add-chapter', {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json',
+      },
+      body: JSON.stringify({
+        titleSlug,
+        chapterContent,
+      }),
+    });
+    Router.reload();
   };
 
   return (
     <form onSubmit={addChapter} className='my-4 mx-2'>
-      <div className='mb-2 text-lg'>Novel title: {novel.title}</div>
-      <div className='flex flex-col mb-4'>
-        <p>Chapter no: {chapterNo}</p>
-      </div>
+      <div className='mb-2 text-lg'>Novel title: {title}</div>
+      <div className='flex flex-col mb-4'>Chapter no: {chapterNo}</div>
       <div className='flex flex-col mb-4'>
         <label className='mb-2 text-lg' htmlFor='chapterContent'>
           Chapter Content:
         </label>
         <textarea
           className='p-1'
-          name='chapterContent'
           id='chapterContent'
           onChange={e => setChapterContent(e.target.value)}
           rows={10}
         ></textarea>
       </div>
       <div className='flex justify-around'>
-        <button
-          className='p-1 w-20 text-lg bg-primary text-primary-text'
-          type='reset'
-          onClick={() => close()}
-        >
-          Close
-        </button>
         <button
           className='p-1 w-20 text-lg bg-primary text-primary-text'
           type='submit'
--- components/UserPageComponents/my-novels.tsx
+++ components/UserPageComponents/my-novels.tsx
@@ -1,15 +1,17 @@
 import { useContext, useState } from 'react';
 import Collapsible from 'react-collapsible';
 import { ProfileContext } from '../../pages/user';
-import AddChapter from '../add-chapter';
+import AddChapter from './add-chapter';
 
 export default function MyNovels() {
   const {
     profile: { novels },
   } = useContext(ProfileContext);
-  const [showAddChapter, setShowAddChapter] = useState(false);
-  const [currentChapterNo, setCurrentChapterNo] = useState<number>(0);
-  const [currentNovel, setCurrentNovel] = useState({ id: '', title: '' });
+  const [chapter, setChapter] = useState<{
+    titleSlug: string;
+    title: string;
+    chapterNo: number;
+  } | null>(null);
 
   if (novels.length == 0) return <p>No novels yet. Go to create novel</p>;
 
@@ -18,23 +20,32 @@ export default function MyNovels() {
     <div>
       {novels.map(novel => (
         <Collapsible key={novel.title_slug} trigger={novel.title}>
+          <p>Total no of chapters: {novel.no_of_chapters}</p>
           <button
             className='mt-3 p-1 w-fit text-base bg-primary text-primary-text'
             onClick={() => {
-              setShowAddChapter(true);
+              setChapter({
+                titleSlug: novel.title_slug,
+                title: novel.title,
+                chapterNo: novel.no_of_chapters + 1,
+              });
             }}
           >
             Add Chapter
           </button>
         </Collapsible>
       ))}
-      {showAddChapter ? (
-        <AddChapter
-          chapterNo={currentChapterNo}
-          novel={currentNovel}
-          close={() => setShowAddChapter(false)}
-        />
-      ) : null}
+      {chapter && (
+        <div>
+          <button
+            className='bg-secondary text-primary-text p-2 my-5'
+            onClick={() => setChapter(null)}
+          >
+            Close
+          </button>
+          <AddChapter novel={chapter} />
+        </div>
+      )}
     </div>
   );
 }
